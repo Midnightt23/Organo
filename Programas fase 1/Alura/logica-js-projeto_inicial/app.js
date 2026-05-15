6bb8f0daefdfe30b35c3cca8358a114a6b3864d5
Jean Passos <jean.passos@nutriz.local>
2026-05-15 16:08:46 -0300

    Corrigindo estrutura

--- Programas fase 1/Alura/logica-js-projeto_inicial/app.js
+++
@@ -1,6 +0,0 @@
-let nome = prompt ("Digite o seu nome?");
-let idade = prompt ("Digite sua idade");
-
-if (idade > 18) {
-    alert ("Voce pode tirar sua habilitação");
-}
--- Programas fase 1/Aulas de C/Sorteador de times/Iniciando.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main (){
-    printf("Meu nome é Michell\n");
-    
-}
